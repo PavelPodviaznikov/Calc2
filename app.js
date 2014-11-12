4fd89f1167f88fa68dda26f28274a93517661919
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-11-12 13:48:02 +0200

    2.0

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser());
 
-app.get('/index.html', function (req, res) {
-	res.send("hello");
-})
+app.get('/', function (req, res) {
+	var html = '<form action="/" method="post">'+
+					'a: '+
+					'<input type="text" name="a">'+
+					'<br>'+
+					'b: '+
+					'<input type="text" name="b">'+
+					'<br>'+
+					'operator: '+
+					'<input type="text" name="operator">'+
+					'<br>'+
+					'<button type="submit">Calculate</button>'+
+				'</form>';
+	res.send(html);
+});
+app.post('/', function (req, res) {
+	var a = req.body.a;
+	var b = req.body.b;
+	var operator = req.body.operator;
+	var html = '<form action="/" method="post">'+
+					'a: '+
+					'<input type="text" name="a">'+
+					'<br>'+
+					'b: '+
+					'<input type="text" name="b">'+
+					'<br>'+
+					'operator: '+
+					'<input type="text" name="operator">'+
+					'<br>'+
+					'<button type="submit">Calculate</button>'+
+				'</form>'+
+				'ANSWER = '+calculator(parseInt(a),parseInt(b),operator);
+	res.send(html);
+});
 
 var server = app.listen(3000, function () {
 
